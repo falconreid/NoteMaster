55b18cb4d16ad6554312e36949be9bb01a554d55
Joel Terrell <falconreid@hotmail.com>
2020-09-04 11:07:51 -0600

    added basic files, server.js, apiroutes, htmlroutes, etc

---
+++ routes/apiRoutes.js
@@ -0,0 +1,27 @@
+// LOAD DATA
+
+var noteArray = require("../db/db.json");
+
+// ROUTING
+
+module.exports = function (app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/notes", function (req, res) {
+    res.json(noteArray);
+  });
+  console.log(noteArray);
+  // API POST Requests
+
+  app.post("/api/notes", function (req, res) {
+    // - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+  });
+
+  app.delete("/api/notes", function (req, res) {
+    // * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+  });
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
