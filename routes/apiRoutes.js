3fd9028d4a8f5b5d9594d9ebd89b5e3ad8917aa8
Joel Terrell <falconreid@hotmail.com>
2020-09-08 06:23:04 -0600

    style changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,10 +6,6 @@ var noteArray = require("../db/db.json");
 
 module.exports = function (app) {
   // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
   app.get("/api/notes", function (req, res) {
     res.json(noteArray);
